158fba1bda85c356a23614b948d4452f47fae924
vitor-gercov <gercovvitor@gmail.com>
2024-11-25 23:20:33 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'site-espelho';
+export class AppComponent implements OnInit {
+  @ViewChild('video', { static: true }) videoElementRef!: ElementRef<HTMLVideoElement>;
+  camData: any = null;
+
+  ngOnInit(): void {
+    this.askPermission();
+  }
+
+  askPermission(): void {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((response) => {
+        const video = this.videoElementRef.nativeElement
+        video.srcObject = response;
+        video.play();
+      });
+  }
+
+  openGithubRepo(): void {
+    window.open('', '_blank');
+  }
 }
